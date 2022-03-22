d218fb61023d375d1fa99c218e27c455ca7299e0
storezhang <storezhang@gmail.com>
2022-03-22 13:49:28 +0800

    增加正则匹配

--- option_regex.go
+++ option_regex.go
@@ -6,22 +6,22 @@ import (
 
 var (
 	_            = Regex
-	_ walkOption = (*optionRegex)(nil)
+	_ walkOption = (*optionRegexp)(nil)
 )
 
-type optionRegex struct {
-	regex string
+type optionRegexp struct {
+	regexp string
 }
 
 // Regex 正则匹配
-func Regex(regex string) *optionRegex {
-	return &optionRegex{
-		regex: regex,
+func Regex(regex string) *optionRegexp {
+	return &optionRegexp{
+		regexp: regex,
 	}
 }
 
-func (r *optionRegex) applyWalk(options *walkOptions) {
+func (r *optionRegexp) applyWalk(options *walkOptions) {
 	options.matchable = func(path string) (matched bool, err error) {
-		return regexp.MatchString(r.regex, path)
+		return regexp.MatchString(r.regexp, path)
 	}
 }
