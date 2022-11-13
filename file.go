6a0628b0aafa31c4d265be132ab98bbedfdd89d3
storezhang <storezhang@gmail.com>
2022-11-13 18:54:28 +0800

    feat(文件): 增加移动文件

--- file.go
+++ file.go
@@ -9,6 +9,7 @@ import (
 var (
 	_ = Create
 	_ = Rename
+	_=Move
 	_ = Delete
 )
 
@@ -65,6 +66,11 @@ func Rename(from string, to string) (err error) {
 	}
 
 	return
+}
+
+// Move 移动文件
+func Move(from string, to string) error{
+	return Rename(from,to)
 }
 
 // Delete 删除文件或者目录
